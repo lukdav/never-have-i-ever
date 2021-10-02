b368805bfb7ad31308e6cf06324484c47a9a683c
lukdav <luke-davies@outlook.com>
2021-10-02 11:17:00 +0000

    Add Submit Form List functionality

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,36 @@
-$(document).ready(function(){
-    $('.sidenav').sidenav({edge: "right"});
-    $('.collapsible').collapsible();
+$(document).ready(function () {
+  $('.sidenav').sidenav({
+    edge: "right"
   });
+  $('.collapsible').collapsible();
+});
+
+
+document.getElementById("add_requirements").onclick = function () {
+
+  var node = document.createElement("Li");
+  var text = document.getElementById("new_requirements").value;
+  var textnode = document.createTextNode(text);
+  node.appendChild(textnode);
+  document.getElementById("game_requirements").appendChild(node);
+  document.getElementById("new_requirements").value="";
+}
+
+document.getElementById("add_rule").onclick = function () {
+
+  var node = document.createElement("Li");
+  var text = document.getElementById("new_rule").value;
+  var textnode = document.createTextNode(text);
+  node.appendChild(textnode);
+  document.getElementById("game_rules").appendChild(node);
+  document.getElementById("new_rule").value="";
+}
+
+
+
+
+
+// document.getElementById("add_rule").addEventListener("keydown", function (event) {
+//   if (event.key == "Enter") {
+//     submitGame();
+//   }
