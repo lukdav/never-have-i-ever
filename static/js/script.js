72f7034b944e1c3a6ff968a1343d345e8f3e17f0
lukdav <luke-davies@outlook.com>
2021-10-03 10:05:53 +0000

    Add ScrollSpy navigation

--- static/js/script.js
+++ static/js/script.js
@@ -4,7 +4,8 @@ $(document).ready(function () {
   });
   $('.collapsible').collapsible();
   $('select').formSelect();
-  
+  $('.scrollspy').scrollSpy();
+
   validateMaterializeSelect();
     function validateMaterializeSelect() {
         let classValid = { "border-bottom": "1px solid #4caf50", "box-shadow": "0 1px 0 0 #4caf50" };
